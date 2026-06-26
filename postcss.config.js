a92ad556af1972b8be3257aafc508b97b7de4f9d
DC John <dev@dc-john.com>
2026-06-26 21:03:24 +0530

    fix: Restore original postcss configuration to fix styles and delete sanity.cli.ts

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
+export default {
+  plugins: [tailwindcss, autoprefixer],
+}
--- sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import { defineCliConfig } from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '6w0t9e7l',
-    dataset: 'production'
-  }
-})
